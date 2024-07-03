b33c18acc329431468134d7789c19bce6bfcb919
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-07-03 17:20:16 -0300

    Aula dia 03/07/24

---
+++ JavaScript/Aula09/arrays.js
@@ -0,0 +1,103 @@
+const prompt = require('readline-sync');
+
+//*Arrays
+
+//Definindo array vazio
+// const array = [];
+
+//Definindo array
+const notasAluno = [10, 8, 5];
+console.log(notasAluno[0]);
+
+//Definindo array com várias informações
+const pessoa = ["Nayara", 31, "Campinas", "Desenvolvedora Full Stack - Júnior"];
+
+//Mostrar todas as informações dentro do Array
+for (let i = 0; i < pessoa.length; i++) {
+    console.log(pessoa[i]);
+  }
+
+console.log('-----------------------------')
+
+//Outra opção para mostrar todas as opções do array
+console.log(pessoa[0]); 
+console.log(pessoa[1]);
+console.log(pessoa[2]); 
+console.log(pessoa[3]); 
+
+console.log('-----------------------------')
+
+//* Alterando Array
+pessoa[0] = 'Karine';
+console.log(pessoa);
+
+console.log('-----------------------------')
+
+// *Obtendo tamanho do array
+console.log(pessoa.length);
+
+//* Fatiamento de Arrays
+const numero = [40, 34, 58, 78, 96, 97];
+
+//Nesse caso ele vai imprimir todos arrays
+console.log(numero.slice(0));
+
+console.log('-----------------------------')
+
+//Nesse caso ele vai imprimir na posição 0 e 1 (Explicação ele vai imprimir o que for menor que 2)
+console.log(numero.slice(0, 2));
+
+console.log('-----------------------------')
+
+//*Adicionando elementos no final do Array
+numero.push(0);
+console.log(numero);
+
+console.log('-----------------------------')
+
+//*Adicionando elementos no início do Array
+numero.unshift(-9);
+console.log(numero);
+
+console.log('-----------------------------')
+
+//*Remover elemento que está no final do Array, no caso é o 0
+numero.pop();
+console.log(numero);
+
+console.log('-----------------------------')
+
+//*Remover o primeiro elementos do Array, no caso é o -9
+numero.shift();
+console.log(numero);
+
+console.log('-----------------------------')
+//Será que o elemento 20 existe no array numero
+console.log(numero.includes(20));
+console.log(numero.includes(40));
+
+if (numero.includes(20)) {
+    console.loh("Existe o número 20 dentro do array.")
+} else {
+    console.log("Não existe o número 20 dentro do array.")
+}
+
+//* indexOf
+const indiceElemento20 = numero.indexOf(20);
+console.log(indiceElemento20)
+
+
+//!Percorrendo Arrays com o for
+
+const arr = [45, 36,90 ,76, 7, 32]
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+
+// * for-of
+
+for (const elemento of arr) {
+    console.log(elemento);
+}
---
+++ JavaScript/Aula09/script.js
@@ -0,0 +1,105 @@
+
+const prompt = require('readline-sync');
+
+//Expressão 1: inicializaçãi da variável de controle
+
+//Expressão 2: condição de permanência do for
+
+//Expressão 3: o incremento da variável de controle
+
+/* for (expressao 1; expressao 2; expressao 3) {
+        o escopo for
+ }
+        
+ */
+
+//For permite que declare a variável dentro dele, porém será usado apenas dentro do for
+
+
+// for ( let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+//Lmpando o console
+// console.clear();
+
+
+
+//!Exemplo
+
+/*
+let maiorNumero = 0;
+let numeroInformado;
+
+for (let i = 1; i <= 5; i++) {
+    numeroInformado = Number(prompt.question('Informe um número posivito: '));
+
+    if(numeroInformado > maiorNumero) {
+        maiorNumero = numeroInformado;
+    }
+}
+
+console.log('Maior número: ', maiorNumero)
+
+*/
+
+
+//!Exemplo percorrendo String com for
+
+const nome = 'Nayara';
+
+//Nesse caso vai retornar a letra correspondente ao número do array
+console.log(nome[2]);
+
+console.log('-----------------------------------')
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+}
+
+console.log('-----------------------------------')
+
+
+//-----------------------------------------------------
+
+//!Exemplos diversos:
+
+//Nesse caso vai retornar q quantidade de letras que tem o nome
+console.log(nome.length);
+console.log('-----------------------------------')
+
+//-----------------------------------------------------
+
+//Nesse caso vai retornar o nome em letras maiúscula
+console.log(nome.toUpperCase());
+console.log('-----------------------------------')
+
+//-----------------------------------------------------
+
+//Nesse caso vai retornar o nome em letras minúscula
+console.log(nome.toLocaleLowerCase());
+console.log('-----------------------------------')
+
+//-----------------------------------------------------
+
+//Nesse caso vai retornar a primeira letra do nome 
+console.log(nome.charAt());
+console.log('-----------------------------------')
+
+//-----------------------------------------------------
+
+//Nesse caso vai retornar quantas letras 'o' tem na frase
+
+var frase = "o homem é o lobo do homem";
+var letra = "o";
+var quantidade = 0
+
+for (var i = 0; i < frase.length; i++) {
+  if (frase[i] == letra) {
+    quantidade++
+  }
+}
+
+console.log(quantidade)
+
+//-----------------------------------------------------
