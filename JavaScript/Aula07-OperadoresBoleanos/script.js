54f86abfd98c0eb02a3b831dd65f90bc789c831e
Nayara Karine <nayara.karine@probrain.com.br>
2024-11-26 17:04:45 -0300

    laços

--- JavaScript/Aula07-OperadoresBoleanos/script.js
+++ JavaScript/Aula07-OperadoresBoleanos/script.js
@@ -1,26 +1,26 @@
-//Operadores Boleanos sempre retornam verdadeiro ou falso
+//! Operadores Boleanos sempre retornam verdadeiro ou falso
 
 const numero1 = 10;
 const numero2 = 12;
 
-console.log(numero1 == numero2);
-console.log(numero1 === numero2);
+console.log(numero1 == numero2); //Comparar se é igual
+console.log(numero1 === numero2); //Comparar se é idêncito
 
 //Esse 10 é um texto então será falso o resultado
 //Nesse caso ele vai comparar o tipo e também o valor (conteúdo)
 console.log(numero1 === "10");
 
-//-------Comparação com dois iguais compara o valor e não o tipo
+//*-------Comparação com dois iguais compara o valor e não o tipo
 console.log(numero1 == "10");
 
-//----------------Compararção se é diferente
+//*----------------Comparação se é diferente
 console.log(numero1 !== numero2);
 
-//-----------------Comparaçãpo de tipo
+//*-----------------Comparaçãpo de tipo
 // nesse caso ele vai dar true porque é um tipo diferente do numero1 sendo number e esse sendo String
 console.log(numero1 !== "10");
 
-//-----------------Comparação se é maior ou menor ou igual
+//*-----------------Comparação se é maior ou menor ou igual
 
 console.log(numero1 >= numero2);
 
@@ -31,16 +31,16 @@ console.log(numero1 > numero2);
 console.log(numero1 > numero2);
 
 
-//------------------Conjunções Lógicas
-
-const idadePessoa1 = 20;
+//*------------------Conjunções Lógicas
+// && ||
+const idadePessoa1 = 18;
 const idadePessoa2 = 39;
 
-//Operador and (e)
+//Operador and && (e)
 console.log(idadePessoa1 >= 18 && idadePessoa2 >= 18); 
 //Nesse caso ele vai retornar true porque ambas são maiores de idade.
 
-//----OUTRO EXEMPLO
+//*----OUTRO EXEMPLO
 
 const idadePessoa3 = 16;
 const idadePessoa4 = 39;
@@ -52,12 +52,12 @@ console.log(idadePessoa3 >= 18 && idadePessoa4 >= 18);
 
 //--------------------------------------------------------------
 
-//Operador or (ou)
+//Operador or || (ou)
 console.log(idadePessoa1 >= 18 || idadePessoa2 >= 18);
 //Nesse caso será true. 
 //Sempre retorna verdadeiro, basta um ou outro for verdadeiro para retornar true
 
-//--------------------------Operador de inversão
+//*--------------------------Operador de inversão
 
 //Negação
 console.log(!(true));
--- JavaScript/Aula07-OperadoresBoleanos/tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-console.log(idadePessoa1 >= 18 || idadePessoa2 >= 18);
